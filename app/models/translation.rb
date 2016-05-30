b2d2fe310765e224879871009f2474acdd33a844
Mikko Tuohimaa <mikko.tuohimaa@iki.fi>
2016-05-30 22:36:29 +0300

    Add validation to model: requires unique combination of (text_id, language)

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,2 +1,7 @@
 class Translation < ActiveRecord::Base
+  validates :text_id, presence: true
+  validates :language, presence: true
+  validates :text_id, uniqueness: { scope: [:text_id] }
+
+  @@languages = ['en', 'fi', 'se', 'no', 'dk', 'ru', 'es', 'it', 'fr', 'fl', 'tur']
 end
